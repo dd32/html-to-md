563cc1fcd81464074bbba5672c0fb900bc24ef57
Dennis Snell <dmsnell@xkq.io>
2026-02-01 13:46:48 +0100

    Initial commit

---
+++ html-to-md.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Plugin Name: HTML to Markdown
+ * Author: Dennis Snell <dennis.snell@automattic.com>
+ * Description: Convert HTML documents to Markdown using WordPress’ HTML API.
+ * Version: 2026-02-01
+ */
+
+// Don’t load directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+
