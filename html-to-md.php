53e34007ef9d7de24f9184374c7a75364b3b5f9a
Weston Ruter <weston@ruter.net>
2026-02-12 09:16:41 -0800

    Force the output enhancement template buffer for Markdown requests

--- html-to-md.php
+++ html-to-md.php
@@ -31,17 +31,26 @@ add_filter( 'html_to_markdown_starting_node_finder', fn ( $prev ) =>
 	return false;
 } );
 
-remove_all_filters( 'wp_template_enhancement_output_buffer' );
-add_filter( 'wp_template_enhancement_output_buffer', function ( $output, $original ) {
+add_action( 'init', function () {
 	$has_accept_header = isset( $_SERVER['HTTP_ACCEPT'] );
 	$has_markdown_type = $has_accept_header && 1 === preg_match( '~^text/markdown(?:;|$)~', $_SERVER['HTTP_ACCEPT'] );
 	$has_markdown_query_arg = in_array( $_GET['output_format'] ?? '', array( 'md', 'markdown' ), true );
 
 	if ( ! ( $has_markdown_type || $has_markdown_query_arg ) ) {
-		return  $output;
+		return;
 	}
 
-	header( 'Content-type: text/markdown; charset=utf-8' );
+	// Force the template enhancement output buffer to always be enabled for markdown responses.
+	add_filter( 'wp_should_output_buffer_template_for_enhancement', '__return_true', PHP_INT_MAX );
 
-	return html_to_md( $output );
-}, 1000, 2 );
+	remove_all_filters( 'wp_template_enhancement_output_buffer' );
+
+	add_filter(
+		'wp_template_enhancement_output_buffer',
+		function ( $output ) {
+			header( 'Content-type: text/markdown; charset=utf-8' );
+			return html_to_md( $output );
+		},
+		1000
+	);
+} );
