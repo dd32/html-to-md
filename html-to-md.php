c0ce3714aa39181ef397d2a635ffd5f7ee336179
Dion Hulse <dion@wordpress.org>
2026-04-07 21:21:01 +1000

    Update html-to-md.php
    Co-authored-by: Weston Ruter <westonruter@gmail.com>

--- html-to-md.php
+++ html-to-md.php
@@ -138,11 +138,13 @@ add_action(
 );
 
 /**
- * Get the current URL. No validation of whether the URL is to a resource, or includes arbritrary args.
+ * Gets the current URL.
+ * 
+ * No validation of whether the URL is to a resource, or includes arbitrary args.
  *
- * @return string
+ * @return string Current URL.
  */
-function html_to_md_get_current_url() {
+function html_to_md_get_current_url(): string {
 	$request_uri = isset( $_SERVER['REQUEST_URI'] ) ? wp_unslash( $_SERVER['REQUEST_URI'] ) : '';
 	return home_url( '/' ) . substr( $request_uri, strlen( wp_parse_url( home_url('/'), PHP_URL_PATH ) ) );
 }
